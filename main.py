4ae0b163b731fba7af56e8d3e84aa3f31528f2e0
chambies2015 <65683899+chambies2015@users.noreply.github.com>
2023-05-19 15:10:01 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,217 @@
+import discord
+from discord.ext import commands
+import requests
+import json
+import tokens
+from ampapi.ampapi import AMPAPI
+import aiohttp
+
+# The bot token from Discord Developer Portal
+bot_token = tokens.bot_token
+
+# Initialize the bot
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+# specify the URL endpoint for your game server
+API = AMPAPI("http://localhost:8080/")
+url_login = "http://localhost:8080/API/Core/Login"
+url_start = "http://localhost:8080/API/ADSModule/StartInstance"
+url_stop = "http://localhost:8080/API/ADSModule/StopInstance"
+url_restart = "http://localhost:8080/API/ADSModule/RestartInstance"
+url_Network_Info = "http://localhost:8080/API/ADSModule/GetInstanceNetworkInfo"
+url_Instances_Status = "http://localhost:8080/API/ADSModule/GetInstanceStatuses"
+url_Get_Instance = "http://localhost:8080/API/ADSModule/GetInstance"
+
+ark_instance_id = "2033ec8f-244f-4af2-a568-4fb362448491"
+
+# global variable to store the token
+global token
+token = None
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+
+    # Login on startup
+    login_data = {
+        "username": tokens.username,
+        "password": tokens.password,
+        "token": "",
+        "rememberMe": "true"
+    }
+
+    async with aiohttp.ClientSession() as session:
+        headers = {'Accept': 'application/json'}
+        async with session.post(url_login, json=login_data, headers=headers) as resp:
+            # rest of your code
+
+            if resp.headers['Content-Type'] == 'application/json':
+                loginResult = await resp.json()
+
+                if "success" in loginResult.keys() and loginResult["success"]:
+                    print("Login successful")
+                    API.sessionId = loginResult["sessionID"]
+                    global token
+                    token = loginResult['sessionID']
+
+                    await API.Core_SendConsoleMessageAsync("say Hello Everyone, this message was sent from the Python API!")
+                    currentStatus = await API.Core_GetStatusAsync()
+                    CPUUsagePercent = currentStatus["Metrics"]["CPU Usage"]["Percent"]
+                    print(f"Current CPU usage is: {CPUUsagePercent}%")
+
+                else:
+                    print("Login failed")
+                    print(loginResult)
+
+            else:
+                print(f"Unexpected content type: {resp.headers['Content-Type']}")
+                print(await resp.text())
+
+
+
+@bot.group()
+async def ark(ctx):
+    if ctx.invoked_subcommand is None:
+        await ctx.send('Available Commands:\n- Info\n- Start\n- Stop\n- Restart')
+
+
+@ark.command(name='info')
+async def ark_info(ctx):
+    data = {
+        "InstanceId": ark_instance_id,
+        "SESSIONID": token  # include the token in your requests
+    }
+    headers = {'Content-type': 'application/json', 'Accept': 'text/javascript'}
+    response = requests.post(url_Get_Instance, data=json.dumps(data), headers=headers)
+
+    if response.status_code == 200:
+        response_content = response.content.decode()
+        json_response = json.loads(response_content)
+        running_status = json_response["Running"]
+
+        embed = discord.Embed(title='ARK Survival Evolved Server Details', color=discord.Color.blue())
+        embed.add_field(name='Server IP', value='67.4.158.45', inline=False)
+        embed.add_field(name='Server Port', value='7777', inline=False)
+        embed.add_field(name='Server Name', value='Chambies Private Server', inline=False)
+        embed.add_field(name='Server Password', value='thebois', inline=False)
+        embed.add_field(name='Server Status',
+                        value=f'The Ark server is currently {"running" if running_status else "not running"}.',
+                        inline=False)
+
+        await ctx.send(embed=embed)
+    else:
+        await ctx.send(f'Failed to get server info. HTTP status code: {response.status_code}')
+
+
+@ark.command(name='start')
+async def ark_start(ctx):
+
+    # specify your data here
+    data = {
+        "InstanceName": "ARKSurvivalEvolved01",
+        "SESSIONID": token  # include the token in your requests
+    }
+    headers = {'Content-type': 'application/json', 'Accept': 'text/javascript'}
+
+    response = requests.post(url_start, data=json.dumps(data), headers=headers)
+
+    if response.status_code == 200:
+        await ctx.send('Successfully started the Ark server!')
+    else:
+        await ctx.send(f'Failed to start the server. HTTP status code: {response.status_code}')
+
+
+@ark.command(name='stop')
+async def ark_stop(ctx):
+    # specify your params here
+    data = {
+        "InstanceName": "ARKSurvivalEvolved01",
+        "SESSIONID": token  # include the token in your requests
+    }
+
+    headers = {'Content-type': 'application/json', 'Accept': 'text/javascript'}
+
+    response = requests.post(url_stop, data=json.dumps(data), headers=headers)
+
+    if response.status_code == 200:
+        await ctx.send('Successfully stopped the server!')
+    else:
+        await ctx.send(f'Failed to stop the server. HTTP status code: {response.status_code}')
+
+@ark.command(name='restart')
+async def ark_restart(ctx):
+    # specify your params here
+    data = {
+        "InstanceName": "ARKSurvivalEvolved01",
+        "SESSIONID": token  # include the token in your requests
+    }
+
+    headers = {'Content-type': 'application/json', 'Accept': 'text/javascript'}
+
+    response = requests.post(url_restart, data=json.dumps(data), headers=headers)
+
+    if response.status_code == 200:
+        await ctx.send('Successfully restarted the server!')
+    else:
+        await ctx.send(f'Failed to stop the server. HTTP status code: {response.status_code}')
+
+
+@bot.group()
+async def terraria(ctx):
+    if ctx.invoked_subcommand is None:
+        await ctx.send('Invalid terraria command passed...')
+
+@terraria.command(name='start')
+async def terraria_start(ctx):
+    # specify your data here
+    data = {
+        "InstanceName": "tModLoader1401",
+        "SESSIONID": token  # include the token in your requests
+    }
+    headers = {'Content-type': 'application/json', 'Accept': 'text/javascript'}
+
+    response = requests.post(url_start, data=json.dumps(data), headers=headers)
+
+    if response.status_code == 200:
+        await ctx.send('Successfully started the terraria server!')
+    else:
+        await ctx.send(f'Failed to start the server. HTTP status code: {response.status_code}')
+
+
+@terraria.command(name='stop')
+async def terraria_stop(ctx):
+    # specify your params here
+    data = {
+        "InstanceName": "tModLoader1401",
+        "SESSIONID": token  # include the token in your requests
+    }
+
+    headers = {'Content-type': 'application/json', 'Accept': 'text/javascript'}
+
+    response = requests.post(url_stop, data=json.dumps(data), headers=headers)
+
+    if response.status_code == 200:
+        await ctx.send('Successfully stopped the terraria server!')
+    else:
+        await ctx.send(f'Failed to stop the server. HTTP status code: {response.status_code}')
+
+@terraria.command(name='restart')
+async def terraria_restart(ctx):
+    # specify your params here
+    data = {
+        "InstanceName": "tModLoader1401",
+        "SESSIONID": token  # include the token in your requests
+    }
+
+    headers = {'Content-type': 'application/json', 'Accept': 'text/javascript'}
+
+    response = requests.post(url_restart, data=json.dumps(data), headers=headers)
+
+    if response.status_code == 200:
+        await ctx.send('Successfully restarted the terraria server!')
+    else:
+        await ctx.send(f'Failed to stop the server. HTTP status code: {response.status_code}')
+
+# Run the bot
+bot.run(bot_token)
